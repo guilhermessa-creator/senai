ac56c97cd905f81602b8dd538c8ef48f54b5c486
guilhermessa-creator <guilherme_s_sa@estudante.sesisenai.org.br>
2026-02-27 19:43:57 +0000

    update

---
+++ atividade 1/programa1.py
@@ -0,0 +1,4 @@
+a = 10 #inserir valor 10 a variavel
+b = 20
+c = a + b
+print("a soma dos dois valores",c)
---
+++ atividade 1/programa3.py
@@ -0,0 +1,4 @@
+nome = input("insira o seu nome:")
+numero1 = int(input("insira o primeiro número:"))
+numero2 = int (input("insira o segundo número:"))
+print("o usuário ",nome," obteve a soma dos dois valores d: ",numero1+numero2)
